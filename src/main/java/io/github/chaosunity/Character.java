fd89bf94e39fc216334ed24fc41e4f8ac2b59c36
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-04-02 23:19:22 +0800

    rename to match naming convention

--- src/main/java/io/github/chaosunity/Character.java
+++ src/main/java/io/github/chaosunity/Character.java
@@ -1,15 +1,15 @@
 package io.github.chaosunity;
 
 public enum Character {
-    Colon,
-    TopLeftCornerHard,
-    BottomLeftCornerHard,
-    LeftConnector,
-    HorizontalLine,
-    VerticalLine,
-    Dot,
-    Anchor,
-    AnchorHorizontalLine,
-    AnchorVerticalLine,
-    BottomLeftCornerRound,
+    COLON,
+    TOP_LEFT_CORNER_HARD,
+    BOTTOM_LEFT_CORNER_HARD,
+    LEFT_CONNECTOR,
+    HORIZONTAL_LINE,
+    VERTICAL_LINE,
+    DOT,
+    ANCHOR,
+    ANCHOR_HORIZONTAL_LINE,
+    ANCHOR_VERTICAL_LINE,
+    BOTTOM_LEFT_CORNER_ROUND
 }
