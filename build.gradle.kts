71d78ebe8fff4294eb8bd77bca0cd7e21ab888c2
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-04-02 17:55:21 +0800

    Initial commit (gradle setup)

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    java
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.fusesource.jansi:jansi:2.4.0")
+}
